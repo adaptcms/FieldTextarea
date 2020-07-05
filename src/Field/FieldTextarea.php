2a948b90f0bd2d6fa8fe063541130b344bf9bcd3
Charlie Page <charliepage88@gmail.com>
2020-07-05 13:14:03 -0400

    package field validation, misc ui & api fixes, is_required changes

--- src/Field/FieldTextarea.php
+++ src/Field/FieldTextarea.php
@@ -7,6 +7,25 @@ use Adaptcms\Fields\FieldType;
 class FieldTextarea extends FieldType
 {
   /**
+  * @var array
+  */
+  public $defaultSettings = [
+    'options' => [
+      'is_sortable'        => false,
+      'is_searchable'      => true,
+      'is_required_create' => false,
+      'is_required_edit'   => false
+    ],
+    'action_rules' => [
+      'index'  => false,
+      'create' => true,
+      'edit'   => true,
+      'show'   => true,
+      'search' => true
+    ]
+  ];
+
+  /**
   * Migration Command
   * When a package field is made from this field, you must supply a valid
   * migration string in string format.
